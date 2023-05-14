ff6b0235c40f0a0a2a7eae97fd72c9c45b6af5ba
dneupan1 <46495785+dneupan1@users.noreply.github.com>
2023-05-14 16:49:21 -0400

    added static css files for styling the login page

--- django_test/urls.py
+++ django_test/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views import generic
@@ -25,4 +27,4 @@ urlpatterns = [
     path(r'accounts/', include('django.contrib.auth.urls')),
     path(r'register_player/', register_player),
     path(r'', generic.TemplateView.as_view(template_name='home.html'), name='home')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
